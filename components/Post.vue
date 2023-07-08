b3448c8baea004c3a0189a161de0cc225a1c9fc4
nabil alaa <nabilalaa06@gmail.com>
2023-07-09 02:51:01 +0300

    Nuxt-Blog

--- components/Post.vue
+++ components/Post.vue
@@ -1,28 +1,27 @@
 <template>
-	<NuxtLink :to="to" class="block">
+	<div
+		:style="{
+			'background-image': `url(${image})`,
+			'background-size': 'cover',
+			'background-position': 'center'
+		}"
+		class="w-full h-full relative rounded-3xl"
+	>
 		<div
-			:style="{
-				'background-image': `url(${image})`,
-				'background-size': 'cover',
-				'background-position': 'center'
-			}"
-			class="w-full h-full relative rounded-3xl"
+			class="overlay bg-mainColor opacity-50 w-full h-full rounded-3xl"
+		></div>
+		<div
+			class="content absolute opacity-100 bottom-0 right-0 z-80 w-full p-4"
 		>
-			<div
-				class="overlay bg-mainColor opacity-50 w-full h-full rounded-3xl"
-			></div>
-			<div
-				class="content absolute opacity-100 bottom-0 right-0 z-80 w-full p-4"
-			>
-				<h1 class="text-white opacity-100 text-3xl">
-					{{ title }}
-				</h1>
-				<div class="data font-serif text-white">
-					{{ date }}
-				</div>
+			<h1 class="text-white opacity-100 text-3xl">
+				{{ title }}
+			</h1>
+			<div class="data font-serif text-white">
+				{{ date }}
 			</div>
+			<NuxtLink :to="to" class="block text-white">click</NuxtLink>
 		</div>
-	</NuxtLink>
+	</div>
 </template>
 
 <script>
